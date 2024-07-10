491da44048d48541e29555d384cf9e705f91b1d5
Adebanjo Favour Mayowa <favouradebanjo2@gmail.com>
2024-07-10 12:40:22 +0100

    Solving the admin error@

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-
 import { PatientForm } from "@/components/forms/PatientForm";
 import { PasskeyModal } from "@/components/PasskeyModal";
 
